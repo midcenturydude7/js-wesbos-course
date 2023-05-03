c1e00d5220f7d8340cc8c107420d4bcb4a4bd8b5
midCenturyDude7 <mgriffes@gmail.com>
2023-05-03 09:47:12 -0700

    Working with classes

--- dom/the-dom.js
+++ dom/the-dom.js
@@ -1,3 +1,9 @@
-const p = document.querySelector("p");
-const divs = document.querySelectorAll("div");
-console.log(p, divs);
+// const p = document.querySelector("p");
+// const divs = document.querySelectorAll("div");
+// console.log(p, divs);
+
+const pic = document.querySelector(".nice");
+pic.classList.add("dog");
+pic.classList.remove("cool");
+pic.classList.toggle("round");
+console.log(pic.classList);
